c38a1a94b73c8f559a85e4e542d3d84c659156ee
Ashok B <bashok.skk@gmail.com>
2020-08-11 01:34:45 +0530

    init spring boot app

---
+++ src/main/java/com/ashok/restapi/SpringRestfulWebserviceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ashok.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulWebserviceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulWebserviceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashok/restapi/SpringRestfulWebserviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashok.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfulWebserviceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
